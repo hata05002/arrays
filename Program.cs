81de3cb76085cb7f4fa3d29033845a15b53b8d39
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-10-17 09:44:12 +0200

    Add files via upload
    medel temperatur array

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class program{
+    static void Main()
+{
+	double[] temperatur={4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+    
+            double s=0;
+       foreach (double i in temperatur)
+            s+= i ;
+            Console.WriteLine(s);
+            Console.WriteLine(s/temperatur.Length);
+
+    }
+
+    }
